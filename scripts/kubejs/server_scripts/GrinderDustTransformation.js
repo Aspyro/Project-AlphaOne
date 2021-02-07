460b2994baa9270a8a1f128c97944c53de7bce27
Aspyro <37420016+Aspyro@users.noreply.github.com>
2021-02-07 21:03:27 +0100

    Create GrinderDustTransformation.js

---
+++ scripts/kubejs/server_scripts/GrinderDustTransformation.js
@@ -0,0 +1,9 @@
+onEvent('recipes' => event {
+/*
+ *  // In all shapeless crafting recipes, replace any planks with Gold Nugget in input items
+ *  event.replaceInput({type: 'minecraft:crafting_shapeless'}, '#minecraft:planks', 'minecraft:gold_nugget')
+ *  
+ *  // In all recipes, replace Stick with Oak Sapling in output items 
+ *  event.replaceOutput({}, 'minecraft:stick', 'minecraft:oak_sapling')
+ */
+})
