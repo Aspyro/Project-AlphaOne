4dfe03d32a5658e645452597c9e6f9f290b82e20
Aspyro <37420016+Aspyro@users.noreply.github.com>
2021-02-08 09:25:34 +0100

    Update GrinderDustTransformation.js

--- scripts/kubejs/server_scripts/GrinderDustTransformation.js
+++ scripts/kubejs/server_scripts/GrinderDustTransformation.js
@@ -1,9 +1,21 @@
 onEvent('recipes' => event {
-/*
- *  // In all shapeless crafting recipes, replace any planks with Gold Nugget in input items
- *  event.replaceInput({type: 'minecraft:crafting_shapeless'}, '#minecraft:planks', 'minecraft:gold_nugget')
- *  
- *  // In all recipes, replace Stick with Oak Sapling in output items 
- *  event.replaceOutput({}, 'minecraft:stick', 'minecraft:oak_sapling')
- */
+
+  const dustReplace = (first, second) => {
+    event.replaceOutput({}, first, second)
+    event.replaceInput({}, first, second)
+  }
+
+  dustReplace('grinder:gold_dust', 'matex:dustgold')  // Gold Dust
+  dustReplace('grinder:copper_dust', 'matex:dustcopper')  // Copper Dust
+  dustReplace('grinder:tin_dust', 'matex:dusttin')    // Tin Dust
+  dustReplace('grinder:lead_dust', 'matex:dustlead')  // Lead Dust
+  dustReplace('grinder:aluminum_dust', 'matex:dustaluminium') // Alumin(i)um Dust
+  dustReplace('grinder:silver_dust', 'matex:dustsilver')  // Silver Dust
+  dustReplace('grinder:osmium_dust', 'matex:dustosmium')  // Osmium Dust
+  dustReplace('grinder:zinc_dust', 'matex:dustzinc')  // Zinc Dust
+  dustReplace('grinder:tungsten_dust', 'matex:dusttungsten')  // Tungsten Dust
+  dustReplace('grinder:platinum_dust', 'matex:dustplatinum')  // Platinum Dust
+  dustReplace('grinder:nickel_dust', 'matex:dustnickel')  // Nickel Dust
+  dustReplace('grinder:uranium_dust', 'matex:dusturanium')  // Uranium Dust
+  
 })
