db0bdd94c6bb05bc9455cb0f3bb523f079c95847
Aspyro <37420016+Aspyro@users.noreply.github.com>
2021-02-09 11:39:44 +0100

    replaced tags with the matex equivalent

--- scripts/kubejs/server_scripts/GrinderDustTransformation.js
+++ scripts/kubejs/server_scripts/GrinderDustTransformation.js
@@ -1,21 +1,15 @@
 onEvent('recipes', event => {
 
-  const dustReplace = (first, second) => {
+  const dustReplace = (first, second, tag) => {
     event.replaceOutput({}, first, second)
+    event.replaceInput({}, tag, second)
     event.replaceInput({}, first, second)
   }
 
-  dustReplace('grinder:gold_dust', 'matex:dustgold')  // Gold Dust
-  dustReplace('grinder:copper_dust', 'matex:dustcopper')  // Copper Dust
-  dustReplace('grinder:tin_dust', 'matex:dusttin')    // Tin Dust
-  dustReplace('grinder:lead_dust', 'matex:dustlead')  // Lead Dust
-  dustReplace('grinder:aluminum_dust', 'matex:dustaluminium') // Alumin(i)um Dust
-  dustReplace('grinder:silver_dust', 'matex:dustsilver')  // Silver Dust
-  dustReplace('grinder:osmium_dust', 'matex:dustosmium')  // Osmium Dust
-  dustReplace('grinder:zinc_dust', 'matex:dustzinc')  // Zinc Dust
-  dustReplace('grinder:tungsten_dust', 'matex:dusttungsten')  // Tungsten Dust
-  dustReplace('grinder:platinum_dust', 'matex:dustplatinum')  // Platinum Dust
-  dustReplace('grinder:nickel_dust', 'matex:dustnickel')  // Nickel Dust
-  dustReplace('grinder:uranium_dust', 'matex:dusturanium')  // Uranium Dust
-  
+  dustReplace('grinder:gold_dust', 'matex:dustgold', '#forge:dusts/gold')  // Gold Dust
+  dustReplace('grinder:copper_dust', 'matex:dustcopper', '#forge:dusts/copper')  // Copper Dust
+  dustReplace('grinder:tin_dust', 'matex:dusttin', '#forge:dusts/tin')    // Tin Dust
+  dustReplace('grinder:lead_dust', 'matex:dustlead', '#forge:dusts/lead')  // Lead Dust
+  dustReplace('grinder:silver_dust', 'matex:dustsilver', '#forge:dusts/silver')  // Silver Dust
+  dustReplace('grinder:osmium_dust', 'matex:dustosmium', '#forge:dusts/silver')  // Osmium Dust
 })
